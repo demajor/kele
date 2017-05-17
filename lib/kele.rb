18f3a22f746bb6b4324645ecaef6380f7cae1901
Doug Major <demajor@demajorMBP.local>
2017-05-17 11:53:05 -0700

    user story 1 client init and auth: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,11 @@
 require 'httparty'
-# require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "email": email, "password": password })
-    raise "invalid email or password" if response.code != 200
+    raise response.code if response.code != 200
     @auth_token = response["auth_token"]
   end
-
-  # def get_me
-  #   url = "https://www.bloc.io/api/v1/users/me"
-  #   response = self.class.get(url, headers: { "authorization" => @auth_token })
-  #   body = JSON.parse(response.body)
-  # end
 end
