fc06d7089346189a033006bd31ae5ee42b14de8d
Doug Major <demajor@demajorMBP.local>
2017-05-23 12:39:07 -0700

    user story 3 mentor availibility: in progress

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,4 +14,23 @@ class Kele
     response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
     body = JSON.parse(response.body)
   end
+
+# mark mentor_id = 2290632
+
+  # def get_mentor_availability(mentor_id)
+  #   response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token }).to_a
+  #   available = []
+  #   response.each do |timeslot|
+  #     if timeslot["booked"] == nil
+  #       available.push(timeslot)
+  #     end
+  #   end
+  #   puts available
+  # end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", values: { id: mentor_id }, headers: { authorization: @auth_token })
+    @mentor_avail = JSON.parse response.body
+  end
+
 end
