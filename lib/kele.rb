ff3390193c60b135c5914efce10898d06caeea14
Doug Major <demajor@demajorMBP.local>
2017-05-31 11:59:31 -0700

    user story 6 submission: in progress

--- lib/kele.rb
+++ lib/kele.rb
@@ -37,13 +37,24 @@ class Kele
 
   def create_message(sender, recipient_id, subject, stripped_text)
     response = self.class.post("https://www.bloc.io/api/v1/messages",
-      { body: { sender: sender, recipient_id: recipient_id, subject: subject, "stripped-text" => stripped_text }, headers: { "authorization" => @auth_token }})
+    { body: { sender: sender, recipient_id: recipient_id, subject: subject, "stripped-text" => stripped_text }, headers: { "authorization" => @auth_token }})
   end
 
   # def create_message(sender, recipient_id, subject, stripped_text)
   #   values_headers = { body: { sender: sender, recipient_id: recipient_id, subject: subject, "stripped-text" => stripped_text}, headers: { authorization: @auth_token } }
   #   response = self.class.post("https://www.bloc.io/api/v1/messages", values_headers)
   # end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", 
+    { body: { enrollment_id: user_enrollment_id, checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment }, headers: { "authorization" => @auth_token }})
+  end
+
+  private
+
+  def user_enrollment_id
+    @user['current_enrollment']['id']
+  end
 
 end
 
